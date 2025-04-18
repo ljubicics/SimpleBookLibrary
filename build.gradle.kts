633079b2d462290a6a2ced12b1ad21cc33bfac9e
Strahinja Ljubicics <strahinja.ljubicic@gmail.com>
2025-04-18 21:19:42 +0200

    Add detekt to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,22 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+val configFile = files("$rootDir/quality/detekt_config.yml")
+val kotlinFiles = "**/*.kt"
+val resourceFiles = "**/resources/**"
+val buildFiles = "**/build/**"
+
+detekt {
+    source.setFrom(projectDir)
+
+    parallel = true
+
+    config.setFrom(configFile)
+
+    buildUponDefaultConfig = true
+    ignoreFailures = false
+
 }
