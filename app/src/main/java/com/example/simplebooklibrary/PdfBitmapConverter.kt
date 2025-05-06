287659ab6828a9791f6c57c2c5aa02a2477f0fac
Strahinja Ljubicic <119794666+ljubicics@users.noreply.github.com>
2025-05-06 13:05:05 +0200

    Pdf screen optimisation (#9)

--- app/src/main/java/com/example/simplebooklibrary/PdfBitmapConverter.kt
+++ app/src/main/java/com/example/simplebooklibrary/PdfBitmapConverter.kt
@@ -21,49 +21,67 @@ class PdfBitmapConverter(
     private val context: Context
 ) {
     private var renderer: PdfRenderer? = null
+    private var bookNumberOfPages: Int = 0
 
+    @Suppress("TooGenericExceptionCaught")
     suspend fun pdfToBitmaps(
         contentUri: Uri,
-        startPage: Int = 0,
-        endPage: Int = 0,
+        page: Int = 0,
         isDarkModeActive: Boolean = false
     ): List<Bitmap> {
+        val startPage = page * 5
+
+        var endPage = startPage + 5
+
         return withContext(Dispatchers.IO) {
             renderer?.close()
-
             context
                 .contentResolver
                 .openFileDescriptor(contentUri, READ_MODE)
                 ?.use { descriptor ->
                     with(PdfRenderer(descriptor)) {
                         renderer = this
+                        bookNumberOfPages = this.pageCount
+
+                        if (bookNumberOfPages == 0) {
+                            return@withContext emptyList()
+                        }
+
+                        if (endPage > bookNumberOfPages && bookNumberOfPages > 0) {
+                            endPage = bookNumberOfPages
+                        }
 
                         return@withContext (startPage until endPage).map { index ->
-                            openPage(index).use { page ->
-                                val bitmap = createBitmap(page.width * 3, page.height * 3)
+                            try {
+                                openPage(index).use { page ->
+                                    val bitmap = createBitmap(page.width * 3, page.height * 3)
 
-                                Canvas(bitmap).apply {
-                                    drawColor(Color.WHITE)
-                                    drawBitmap(
+                                    Canvas(bitmap).apply {
+                                        drawColor(Color.WHITE)
+                                        drawBitmap(
+                                            bitmap,
+                                            BITMAP_X_START_POINT,
+                                            BITMAP_Y_START_POINT,
+                                            null
+                                        )
+                                    }
+
+                                    page.render(
                                         bitmap,
-                                        BITMAP_X_START_POINT,
-                                        BITMAP_Y_START_POINT,
-                                        null
+                                        null,
+                                        null,
+                                        PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
                                     )
-                                }
 
-                                page.render(
-                                    bitmap,
-                                    null,
-                                    null,
-                                    PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
-                                )
-
-                                if (isDarkModeActive) {
-                                    return@map invertBitmapColors(bitmap)
-                                } else {
-                                    return@map bitmap
+                                    if (isDarkModeActive) {
+                                        return@map invertBitmapColors(bitmap)
+                                    } else {
+                                        return@map bitmap
+                                    }
                                 }
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                                return@map createBitmap(100, 100)
                             }
                         }
                     }
